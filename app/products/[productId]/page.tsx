aea20e325856d3982036b64af665e52e3bd37012
Teja Poosa <tejapoosa123@gmail.com>
2026-08-24 22:53:56 +0530

    add: routing metadata

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,8 +1,21 @@
-export default async function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) =>
+    setTimeout(() => resolve(`iPhone ${id}`), 100),
+  );
+  return {
+    title: `Product ${title}`,
+    description: `Details about Product ${id}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return <h1>Details about Product {productId}</h1>;
 }
