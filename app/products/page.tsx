c044f0dca87199bceb13a3eac0b9bde5e833bff5
Teja Poosa <tejapoosa123@gmail.com>
2026-08-24 16:42:56 +0530

    Create page.tsx

---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello from the Product Page</h1>
+      <h2>Product 1</h2>
+      <h2>Product 2</h2>
+      <h2>Product 3</h2>
+    </div>
+  );
+}
