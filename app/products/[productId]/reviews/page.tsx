ee28319c66f58572b219229b8665c8e36204e555
Teja Poosa <tejapoosa123@gmail.com>
2026-08-24 17:22:28 +0530

    add: nested dynamic routes

---
+++ app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,10 @@
+export default function Reviews() {
+  return (
+    <div>
+      <h1>Reviews Page</h1>
+      <h3>Review 1</h3>
+      <h3>Review 2</h3>
+      <h3>Review 3</h3>
+    </div>
+  );
+}
