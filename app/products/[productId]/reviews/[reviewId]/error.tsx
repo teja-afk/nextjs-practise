24ba47d976adc751d37e891f0279e1c9a8cb9a35
Teja Poosa <tejapoosa123@gmail.com>
2026-08-26 23:15:23 +0530

    add: recovering from errors

--- app/products/[productId]/reviews/[reviewId]/error.tsx
+++ app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,26 @@
 "use client";
 
-export default function ErrorBoundary({error}:{error: Error}) {
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
 
-    return <div>{error.message}</div>
-};
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button onClick={reload}>Try again</button>
+    </div>
+  );
+}
