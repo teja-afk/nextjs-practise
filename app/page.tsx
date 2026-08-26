2128d615107bf2ed542a11465be2b2ece0a269ba
Teja Poosa <tejapoosa123@gmail.com>
2026-08-26 20:24:55 +0530

    add: params and searchParams server side render

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <h1>Hello from the Home Page</h1>
       <Link href="/about">About</Link>
       <Link href="/products">Products</Link>
-    </div>
+      <Link href="/articles/breaking-new-123?lang=en">Read in English</Link>
+      <Link href="/articles/breaking-new-123?lang=fr">Read in French</Link>
+    </>
   );
 }
