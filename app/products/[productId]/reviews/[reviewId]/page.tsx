ad61a8466684e019326ef4c42f44aa849ada8a34
Teja Poosa <tejapoosa123@gmail.com>
2026-08-26 23:03:20 +0530

    add: error handling

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,19 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ReviewDetails({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
+  const random = getRandomInt(2);
+
+  if(random === 1){
+    throw new Error("Error loading review");
+  }
   const { productId, reviewId } = await params;
   if(parseInt(reviewId) > 1000) {
     // notFound();
