aa8d588afd570cb9b4a43aa593dd0db21964c5db
Teja Poosa <tejapoosa123@gmail.com>
2026-08-26 20:45:47 +0530

    add: navigating programmatically

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function ReviewDetails({
   params,
@@ -7,7 +7,8 @@ export default async function ReviewDetails({
 }) {
   const { productId, reviewId } = await params;
   if(parseInt(reviewId) > 1000) {
-    notFound();
+    // notFound();
+    redirect("/products");
   }
   return (
     <h1>
